28bc31106de6bfe67c79fbde4581771050b7b79a
Wooden-Robot <woodenrobot1993@gmail.com>
2022-08-18 16:05:35 +0800

    Chg: 优化群晖 ups 真关机脚本

--- Synology/synoups_poweroff_patch.sh
+++ Synology/synoups_poweroff_patch.sh
@@ -7,19 +7,14 @@ vAction=$1
 # Function List	*******************************************************************************
 function install() {
     #备份 /usr/syno/bin/synoups
-    mv -n /usr/syno/bin/synoups /usr/syno/bin/synoups.orig
-    #下载 /usr/syno/bin/synoups 脚本
-    wget -O - https://gist.githubusercontent.com/Wooden-Robot/f87814c7d93edd7bee7d1747634546b3/raw/a99bf11290a452d98c11af3de569ea31d960babc/synoups-wrapper >/usr/syno/bin/synoups
-    #设置脚本相应权限
-    chown root:root /usr/syno/bin/synoups
-    chmod 755 /usr/syno/bin/synoups
+    cp -n /usr/syno/bin/synoups /usr/syno/bin/synoups.orig
+    sed -i 's/\/usr\/syno\/sbin\/synopoweroff -fr/# \/usr\/syno\/sbin\/synopoweroff -fr\n    poweroff/g' /usr/syno/bin/synoups
 
     echo 'UPS 断电真关机补丁安装成功'
 }
 function uninstall() {
     #恢复之前备份的 /usr/syno/bin/synoups 文件
     mv -f /usr/syno/bin/synoups.orig /usr/syno/bin/synoups
-
     echo 'UPS 断电真关机补丁卸载成功'
 }
 
